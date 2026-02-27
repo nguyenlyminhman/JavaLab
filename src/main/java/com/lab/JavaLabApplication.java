b5673fd343838b31d6f36c529af6f2b38235025e
man nguyen <minhman2.nguyen@mafc.com.vn>
2026-02-27 16:03:26 +0700

    kickoff

---
+++ src/main/java/com/lab/JavaLabApplication.java
@@ -0,0 +1,13 @@
+package com.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabApplication.class, args);
+	}
+
+}
