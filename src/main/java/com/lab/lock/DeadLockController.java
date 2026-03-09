d1dededc0a0ac3fe4819325974e555ae9f2d11d3
man nguyen <minhman2.nguyen@mafc.com.vn>
2026-03-09 13:35:00 +0700

    test deadlock

--- src/main/java/com/lab/lock/DeadLockController.java
+++ src/main/java/com/lab/lock/DeadLockController.java
@@ -2,9 +2,11 @@ package com.lab.lock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/deadlock")
+@RestController
+@RequestMapping("/deadlock")
 public class DeadLockController {
 
     @Autowired
